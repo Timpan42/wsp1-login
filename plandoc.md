e9c607b5a7a9e092259531c8f1ffce5f6e229e69
Timpan42 <tim.fagerdal@elev.ga.ntig.se>
2023-02-07 15:00:45 +0100

    🚀 del 4 test 2 funkar inte

---
+++ plandoc.md
@@ -0,0 +1,41 @@
+# plan 
+
+## Databas 
+### Log
+- Username (charat)
+- Password (Varchar)
+- id (int)
+
+## Routes
+- index
+- log in 
+- sign in 
+
+## Knapp
+
+### index
+- log in 
+- sign in 
+- log ut
+- profile 
+
+## Sign in 
+Compare get data with existing database data (bcrypt)
+if username is taken 
+- red field
+
+else$
+- add get data to database
+
+## log in 
+Compare get data with existing database data (bcrypt)
+compare username and password in field (tableplus)
+
+if wrong 
+- dont sign in (red field)
+
+else¤
+- log in 
+
+### password rules
+- length 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.njk', { title: 'Express' });
+    res.render('index.njk', { title: 'Login ALC' });
 });
+router.get('/login', function(req, res, next) {
+    res.render('login.njk', { title: 'Log' });
+});
+router.post('/login', function(req, res, next) {
+    const { username, password } = req.body;
+    
+});
+
+router.get('/signin', function(req, res, next) {
+    res.render('signin.njk', { title: 'sign' });
+});
+
 
 module.exports = router;
